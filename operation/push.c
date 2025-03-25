d3b624c9e2d10c44a6ef19b035d5f082ede7add1
Mohcine Ez Zahi <mez-zahi@e3r1p15.1337.ma>
2025-03-25 12:53:41 +0000

    1337_push_swap

--- operation/push.c
+++ operation/push.c
@@ -6,7 +6,7 @@
 /*   By: mez-zahi <mez-zahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 12:08:16 by mez-zahi          #+#    #+#             */
-/*   Updated: 2025/03/25 12:51:45 by mez-zahi         ###   ########.fr       */
+/*   Updated: 2025/03/25 12:53:14 by mez-zahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ static void	push(t_noeud **src, t_noeud **dest)
 void	pa(t_noeud **a, t_noeud **b, bool afch)
 {
 	push(b, a);
-	if (!afch) 
+	if (!afch)
 		write(1, "pa\n", 3);
 }
 
